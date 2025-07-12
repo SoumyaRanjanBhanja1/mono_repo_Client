60b989fac2a46538c724902a261d75f1a0710012
SoumyaRanjanBhana1 <soumyabhanja180@gmail.com>
2025-07-12 18:38:25 +0530

    all mono repo content

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,13 +8,17 @@ import { loadStarsPreset } from "tsparticles-preset-stars";
 const Contact = () => {
   const [form, setForm] = useState({ name: "", email: "", message: "" });
 
-  const handleChange = (e) =>
+  const handleChange = (e) =>{
     setForm({ ...form, [e.target.name]: e.target.value });
+  }
+
+
+  const API="https://mono-repo-server-1.onrender.com"
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post("http://localhost:5000/api/contact", form);
+      await axios.post(`${API}/api/contact`, form);
       toast.success("✅ Message sent successfully!");
       setForm({ name: "", email: "", message: "" });
     } catch (err) {
